e9a95d5009dda91ceb74c961afae997ae291d688
dedalusmax <dedalus123@gmail.com>
2017-10-23 09:16:05 +0200

    socket.io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignallingService } from './shared/services/signalling.service';
+
+const config: SocketIoConfig = { url: 'http://localhost:8988', options: {} };
 
 @NgModule({
   declarations: [
@@ -10,9 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config) 
   ],
-  providers: [],
+  providers: [SignallingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
